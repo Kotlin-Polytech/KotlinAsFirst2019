f5cabbdbfd882ffa58db15e69d4f8d421cf293c5
Slava0135 <40753025+Slava0135@users.noreply.github.com>
2019-09-06 11:25:31 +0300

    Lesson 1 Task 1 edit 2

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -155,9 +155,9 @@ fun triangleKind(a: Double, b: Double, c: Double): Int {
     val longestSide: Double = max(a, max(b, c))
     val expression: Double = sqr(longestSide) - sqr(a + b + c - longestSide)
     when {
-        expression < 0.0 -> 0
-        expression == 0.0 -> 1
-        expression > 0.0 -> 2
+        expression < 0.0 -> return 0
+        expression == 0.0 -> return 1
+        else -> return 2
     }
 }
 
